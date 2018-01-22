b268f8c96b59560ae8015872e2c99bc1dd5985e3
calebharnell <calebharnell@hotmail.com>
2018-01-22 10:26:16 +1000

    once.js solution

---
+++ once.js
@@ -0,0 +1,59 @@
+/*
+Once
+Creates a version of the function that can only be called one time.
+Repeated calls to the modified function will have no effect, returning the value
+from the original call. Useful for initialization functions, instead of having
+to set a boolean flag and then check it later.
+Example:
+let total = 0
+const count = () => ++total;
+let countOnce = once(count);
+countOnce()
+=> 1
+countOnce()
+=> 1
+*/
+
+// Your code here!
+const once = (fn) => {
+  let hasRun = false;
+  let result
+  return function() {
+    if (hasRun) {
+      return result
+    }
+    hasRun = true
+    result = fn()
+    return result
+  }
+
+}
+
+// Check your solution by running these tests: mocha *this_filename*
+const assert = require('assert');
+
+describe('Once', () => {
+  it('returns a function', () => {
+    const exampleOnce = once(() => {})
+    assert.equal(typeof exampleOnce, 'function');
+  });
+  it('only runs once', () => {
+    let total = 0;
+    const count = () => {
+      return ++total;
+    }
+
+    const countOnce = once(count);
+    assert.equal(countOnce(), 1);
+    assert.equal(countOnce(), 1);
+  });
+});
+
+// let result;
+// return function() {
+//     if (fn) {
+//         result = fn.apply(context || this, arguments);
+//         fn = null;
+//     }
+//     return result;
+// };
